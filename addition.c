aa61ac4fabe2ac72ca544baf9017967b3b50e0b4
Ramyabharathi-S <ramyasramyas841@gmail.com>
2025-03-21 11:03:48 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int a,b,c;
+	printf("Enter the value of a=");
+	scanf("%d",&a);
+	printf("Enter the value of b=");
+	scanf("%d",&b);
+	c=a+b;
+	printf("The sum of a and b=%d",c);
+}
